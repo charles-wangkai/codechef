486337d14e3f4e8fecc5171764c1fb24c571ddd6
Kai Wang <charles.wangkai@gmail.com>
2018-10-19 16:30:38 +0800

    Small factorials

---
+++ FCTRL2/Main.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+		for (int tc = 0; tc < t; tc++) {
+			int n = sc.nextInt();
+			System.out.println(solve(n));
+		}
+
+		sc.close();
+	}
+
+	static BigInteger solve(int n) {
+		BigInteger result = BigInteger.ONE;
+		for (int i = 1; i <= n; i++) {
+			result = result.multiply(BigInteger.valueOf(i));
+		}
+		return result;
+	}
+}
