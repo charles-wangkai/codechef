757fa201a07aec27033782c2b476fab97f43297d
Kai Wang <charles.wangkai@gmail.com>
2018-10-19 22:32:03 +0800

    Reverse The Number

---
+++ FLOW007/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+
+			System.out.println(solve(N));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int N) {
+		return Integer.parseInt(new StringBuilder(String.valueOf(N)).reverse().toString());
+	}
+}
