aba3940751f4dc6e4eb2f1ec8ab9b47330c444e4
Kai Wang <charles.wangkai@gmail.com>
2018-10-24 12:00:58 +0800

    Servant

---
+++ FLOW008/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+
+			System.out.println((N < 10) ? "What an obedient servant you are!" : -1);
+		}
+
+		sc.close();
+	}
+}
