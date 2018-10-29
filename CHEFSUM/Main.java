1636a84a20f06c0c0a0b47cb9ccd442cce98fb6a
Kai Wang <charles.wangkai@gmail.com>
2018-10-29 18:04:22 +0800

    Little Chef and Sums

---
+++ CHEFSUM/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			int[] A = new int[N];
+			for (int i = 0; i < A.length; i++) {
+				A[i] = sc.nextInt();
+			}
+
+			System.out.println(solve(A));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int[] A) {
+		return Arrays.stream(A).boxed().collect(Collectors.toList()).indexOf(Arrays.stream(A).min().getAsInt()) + 1;
+	}
+}
