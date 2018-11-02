871a4d1b37b5223cfa45705fff81a415100d760a
Kai Wang <charles.wangkai@gmail.com>
2018-11-02 15:41:45 +0800

    Task for Alexey

---
+++ ALEXTASK/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			int[] A = new int[N];
+			for (int i = 0; i < A.length; i++) {
+				A[i] = sc.nextInt();
+			}
+
+			System.out.println(solve(A));
+		}
+
+		sc.close();
+	}
+
+	static long solve(int[] A) {
+		long result = Long.MAX_VALUE;
+		for (int i = 0; i < A.length; i++) {
+			for (int j = i + 1; j < A.length; j++) {
+				result = Math.min(result, lcm(A[i], A[j]));
+			}
+		}
+		return result;
+	}
+
+	static long lcm(int a, int b) {
+		return (long) a / gcd(a, b) * b;
+	}
+
+	static int gcd(int a, int b) {
+		return (b == 0) ? a : gcd(b, a % b);
+	}
+}
