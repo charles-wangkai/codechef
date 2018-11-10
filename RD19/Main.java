9459892c380e61b660b23a85ebb7139cb244889d
Kai Wang <charles.wangkai@gmail.com>
2018-11-10 18:26:17 +0800

    Minimum Deletions

---
+++ RD19/Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			int[] A = new int[N];
+			for (int i = 0; i < A.length; i++) {
+				A[i] = sc.nextInt();
+			}
+
+			System.out.println(solve(A));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int[] A) {
+		return (Arrays.stream(A).reduce((result, element) -> gcd(result, element)).getAsInt() == 1) ? 0 : -1;
+	}
+
+	static int gcd(int a, int b) {
+		return (b == 0) ? a : gcd(b, a % b);
+	}
+}
