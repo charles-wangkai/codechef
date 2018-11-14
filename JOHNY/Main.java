34045e1bec8be5e2e6e51563047f30af65a6af37
Kai Wang <charles.wangkai@gmail.com>
2018-11-14 18:40:27 +0800

    Uncle Johny

---
+++ JOHNY/Main.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			int[] A = new int[N];
+			for (int i = 0; i < A.length; i++) {
+				A[i] = sc.nextInt();
+			}
+			int K = sc.nextInt();
+
+			System.out.println(solve(A, K));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int[] A, int K) {
+		int target = A[K - 1];
+		Arrays.sort(A);
+
+		for (int i = 0;; i++) {
+			if (A[i] == target) {
+				return i + 1;
+			}
+		}
+	}
+}
