cf5eb83535ffc76312c950ba14c22a559c76183c
Kai Wang <charles.wangkai@gmail.com>
2018-11-14 18:19:49 +0800

    Racing Horses

---
+++ HORSES/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			int[] S = new int[N];
+			for (int i = 0; i < S.length; i++) {
+				S[i] = sc.nextInt();
+			}
+
+			System.out.println(solve(S));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int[] S) {
+		Arrays.sort(S);
+		return IntStream.range(0, S.length - 1).map(i -> S[i + 1] - S[i]).min().getAsInt();
+	}
+}
