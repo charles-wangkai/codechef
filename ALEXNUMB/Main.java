8162f7642eeb0aa10d8eb1164b8d9159bf1e0046
Kai Wang <charles.wangkai@gmail.com>
2018-11-16 17:41:49 +0800

    Magic Pairs

---
+++ ALEXNUMB/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int n = sc.nextInt();
+			int[] a = new int[n];
+			for (int i = 0; i < a.length; i++) {
+				a[i] = sc.nextInt();
+			}
+
+			System.out.println(solve(a));
+		}
+
+		sc.close();
+	}
+
+	static long solve(int[] a) {
+		return (long) a.length * (a.length - 1) / 2;
+	}
+}
