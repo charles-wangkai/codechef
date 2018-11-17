89ec9f79c048a0da2f7efd5f8773a1ac4ef6d03c
Kai Wang <charles.wangkai@gmail.com>
2018-11-17 17:46:15 +0800

    Walk on the Axis

---
+++ ANUWTA/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+
+			System.out.println(solve(N));
+		}
+
+		sc.close();
+	}
+
+	static long solve(int N) {
+		return (long) (N + 1) * (N + 2) / 2 - 1;
+	}
+}
