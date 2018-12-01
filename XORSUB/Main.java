3dd860d13e40372186d581a7374fe5aba75da9e3
Kai Wang <charles.wangkai@gmail.com>
2018-12-01 14:39:37 +0800

    XOR with Subset

---
+++ XORSUB/Main.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			int K = sc.nextInt();
+			int[] A = new int[N];
+			for (int i = 0; i < A.length; i++) {
+				A[i] = sc.nextInt();
+			}
+
+			System.out.println(solve(A, K));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int[] A, int K) {
+		Set<Integer> values = new HashSet<>();
+		values.add(K);
+
+		for (int a : A) {
+			Set<Integer> nextValues = new HashSet<>();
+			for (int value : values) {
+				nextValues.add(value ^ a);
+			}
+			nextValues.addAll(values);
+
+			values = nextValues;
+		}
+
+		return values.stream().mapToInt(x -> x).max().getAsInt();
+	}
+}
