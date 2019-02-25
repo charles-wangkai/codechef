bd6a451e31c3f2ba121a063384da24e5a3c8f872
Kai Wang <charles.wangkai@gmail.com>
2019-02-25 11:05:02 +0800

    Average Number

---
+++ AVG/Main.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			int K = sc.nextInt();
+			int V = sc.nextInt();
+			int[] A = new int[N];
+			for (int i = 0; i < A.length; i++) {
+				A[i] = sc.nextInt();
+			}
+
+			System.out.println(solve(A, K, V));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int[] A, int K, int V) {
+		int extra = V * (A.length + K) - Arrays.stream(A).sum();
+		int value = extra / K;
+
+		if (value > 0 && value * K == extra) {
+			return value;
+		} else {
+			return -1;
+		}
+	}
+}
