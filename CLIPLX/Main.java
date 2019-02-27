a8405d078b44b35b6a1e1b8e76f28fd084022a0b
Kai Wang <charles.wangkai@gmail.com>
2019-02-27 09:34:46 +0800

    IPL and RCB

---
+++ CLIPLX/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int X = sc.nextInt();
+			int Y = sc.nextInt();
+
+			System.out.println(solve(X, Y));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int X, int Y) {
+		return Math.max(0, X - Y);
+	}
+}
