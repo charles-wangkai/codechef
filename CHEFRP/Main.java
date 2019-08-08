efefc27f3ee41c156fb96167757995d73fda97d8
Kai Wang <charles.wangkai@gmail.com>
2019-08-08 13:06:48 +0800

    Chef and new recipe

---
+++ CHEFRP/Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+			int[] A = new int[N];
+			for (int i = 0; i < A.length; i++) {
+				A[i] = sc.nextInt();
+			}
+
+			System.out.println(solve(A));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int[] A) {
+		if (Arrays.stream(A).anyMatch(x -> x < 2)) {
+			return -1;
+		}
+
+		return Arrays.stream(A).sum() - Arrays.stream(A).min().getAsInt() + 2;
+	}
+}
