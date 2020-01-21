3109fd93d6c26dfd4ccc043459c1f7cd76cd92d4
Kai Wang <charles.wangkai@gmail.com>
2020-01-21 15:43:38 +0800

    Chef Chick

---
+++ CHFCHK/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; ++tc) {
+			int N = sc.nextInt();
+			int[] a = new int[N];
+			for (int i = 0; i < a.length; ++i) {
+				a[i] = sc.nextInt();
+			}
+
+			System.out.println(solve(a));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int[] a) {
+		return Arrays.stream(a).min().getAsInt();
+	}
+}
