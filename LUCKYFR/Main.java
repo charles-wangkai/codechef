4fdd957f4176ba1ac3758e96f97ed22856d7162b
Kai Wang <charles.wangkai@gmail.com>
2021-04-06 16:45:09 +0800

    Lucky Four

---
+++ LUCKYFR/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 0; tc < T; ++tc) {
+      int n = sc.nextInt();
+
+      System.out.println(solve(n));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int n) {
+    return (int) String.valueOf(n).chars().filter(ch -> ch == '4').count();
+  }
+}
