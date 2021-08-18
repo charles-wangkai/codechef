bbea721ea0f95dd599cc13bce5ceac101ed8ea1b
Kai Wang <charles.wangkai@gmail.com>
2021-08-18 23:26:56 +0800

    Black cells in a chessboard

---
+++ BLACKCEL/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    System.out.println(solve(n));
+
+    sc.close();
+  }
+
+  static int solve(int n) {
+    return n * n / 2;
+  }
+}
