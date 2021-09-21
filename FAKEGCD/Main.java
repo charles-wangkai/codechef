9bfb7d83da17110e0ea07b95f610ea69ba5a44d1
Kai Wang <charles.wangkai@gmail.com>
2021-09-21 13:27:54 +0800

    Fake GCD

---
+++ FAKEGCD/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 0; tc < T; ++tc) {
+      int N = sc.nextInt();
+
+      System.out.println(solve(N));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int N) {
+    return IntStream.range(0, N)
+        .mapToObj(i -> String.valueOf(N - i))
+        .collect(Collectors.joining(" "));
+  }
+}
