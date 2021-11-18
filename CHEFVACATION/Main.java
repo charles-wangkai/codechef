a3d73aeb64ed0e079e40d4ef294610d7f99ab082
Kai Wang <charles.wangkai@gmail.com>
2021-11-18 09:04:17 +0800

    Chef on Vacation

---
+++ CHEFVACATION/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 0; tc < T; ++tc) {
+      int X = sc.nextInt();
+      int Y = sc.nextInt();
+      int Z = sc.nextInt();
+
+      System.out.println(solve(X, Y, Z) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int X, int Y, int Z) {
+    return X + Y <= Z;
+  }
+}
