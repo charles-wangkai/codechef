8426240f26c5946f51a9011b602524f0333ffd43
Kai Wang <charles.wangkai@gmail.com>
2021-11-24 16:51:32 +0800

    Make them equal

---
+++ MAKEEQUAL/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 0; tc < T; ++tc) {
+      int N = sc.nextInt();
+      int[] A = new int[N];
+      for (int i = 0; i < A.length; ++i) {
+        A[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(A));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] A) {
+    return Arrays.stream(A).max().getAsInt() - Arrays.stream(A).min().getAsInt();
+  }
+}
