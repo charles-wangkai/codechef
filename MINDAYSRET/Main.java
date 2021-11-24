d87323a0bb7b62091a466a0b7c21c037f1d30838
Kai Wang <charles.wangkai@gmail.com>
2021-11-24 16:55:32 +0800

    Back to Campus

---
+++ MINDAYSRET/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 0; tc < T; ++tc) {
+      int N = sc.nextInt();
+      int K = sc.nextInt();
+
+      System.out.println(solve(N, K));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int N, int K) {
+    return (N + K - 1) / K;
+  }
+}
