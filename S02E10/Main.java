98d5007d2caa1af4b057024e778599a464600f40
Kai Wang <charles.wangkai@gmail.com>
2021-12-01 00:44:45 +0800

    The One with Russ

---
+++ S02E10/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 0; tc < T; ++tc) {
+      int N = sc.nextInt();
+      int X = sc.nextInt();
+      int K = sc.nextInt();
+      int[] A = new int[N];
+      for (int i = 0; i < A.length; ++i) {
+        A[i] = sc.nextInt();
+      }
+      int[] B = new int[N];
+      for (int i = 0; i < B.length; ++i) {
+        B[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(A, B, X, K) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] A, int[] B, int X, int K) {
+    return IntStream.range(0, A.length).filter(i -> Math.abs(A[i] - B[i]) <= K).count() >= X;
+  }
+}
