c6eb660f44fff5d93cb4e3d332fb6124d10b8107
Kai Wang <charles.wangkai@gmail.com>
2022-01-22 19:51:12 +0800

    Equal Card Game

---
+++ EQCARDGAME/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 0; tc < T; ++tc) {
+      int K = sc.nextInt();
+
+      System.out.println(solve(K));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int K) {
+    return 52 % K;
+  }
+}
